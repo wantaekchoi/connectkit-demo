3c9682cd68b6bd988a1a1fb7e17f34bcc6bef756
wantaek <wantaek@kakao.com>
2023-09-05 15:28:57 +0900

    use npm

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-// import { WagmiConfig, createConfig } from "wagmi";
-// import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-// import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import { WagmiConfig, createConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
 
-// const config = createConfig(
-//   getDefaultConfig({
-//     appName: 'family connectkit demo',
-//     alchemyId: `${process.env.REACT_APP_ALCHEMY_ID}`,
-//     chains: [mainnet, polygon, optimism, arbitrum],
-//     walletConnectProjectId: `${process.env.REACT_APP_WALLETCONNECT_PROJECT_ID}`,
-//   })
-// );
+const config = createConfig(
+  getDefaultConfig({
+    appName: 'family connectkit demo',
+    alchemyId: `${process.env.REACT_APP_ALCHEMY_ID}`,
+    chains: [mainnet, polygon, optimism, arbitrum],
+    walletConnectProjectId: `${process.env.REACT_APP_WALLETCONNECT_PROJECT_ID}`,
+  })
+);
 
 ReactDOM.render(
   <React.StrictMode>
-  {/* <WagmiConfig config={config}>
+  <WagmiConfig config={config}>
     <ConnectKitProvider>
       <ConnectKitButton />
     </ConnectKitProvider>
-  </WagmiConfig> */}
+  </WagmiConfig>
   <div>ddd</div>
   </React.StrictMode>,
   document.getElementById('root')
