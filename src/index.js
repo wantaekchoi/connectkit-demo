e27980338b1830c8668c4f941a32dc18b460ed5f
wantaek <wantaek@kakao.com>
2023-09-05 17:50:33 +0900

    remove getDefaultConfig

--- src/index.js
+++ src/index.js
@@ -4,28 +4,51 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-import { WagmiConfig, createConfig } from "wagmi";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import { publicProvider } from 'wagmi/providers/public'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 
-const config = createConfig(
-  getDefaultConfig({
-    appName: 'family connectkit demo',
-    alchemyId: `${process.env.REACT_APP_ALCHEMY_ID}`,
-    chains: [mainnet, polygon, optimism, arbitrum],
-    walletConnectProjectId: `${process.env.REACT_APP_WALLETCONNECT_PROJECT_ID}`,
-  })
+import { ConnectKitProvider, ConnectKitButton, } from "connectkit";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, optimism, arbitrum], [publicProvider()])
+const config = createConfig({
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+      shimDisconnect: false,
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        appName: 'connectkit-demo',
+        jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_ID}`,
+        projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID
+      }
+    }),
+    new InjectedConnector({ chains }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+  logger: {
+    warn: (message) => console.warn(message),
+  },
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <WagmiConfig config={config}>
       <ConnectKitProvider>
         <ConnectKitButton />
       </ConnectKitProvider>
     </WagmiConfig>
+    <div>home</div>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 reportWebVitals(sendToVercelAnalytics);
