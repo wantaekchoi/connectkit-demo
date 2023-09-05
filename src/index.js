ac1312ce5734c905e99e9ed000a23d0c47953a62
wantaek <wantaek@kakao.com>
2023-09-05 15:14:56 +0900

    add wallet connect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
+import { WagmiConfig, createConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: 'family connectkit demo',
+    alchemyId: process.env.REACT_APP_ALCHEMY_ID,
+    chains: [mainnet, polygon, optimism, arbitrum],
+    walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+  })
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <WagmiConfig config={config}>
+    <ConnectKitProvider>
+      <ConnectKitButton />
+    </ConnectKitProvider>
+  </WagmiConfig>
   </React.StrictMode>,
   document.getElementById('root')
 );
