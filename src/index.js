ae9e6767fb768fa975b5f14f3a424682ac5f54ac
wantaek <wantaek@kakao.com>
2023-09-05 18:34:05 +0900

    add coinbase

--- src/index.js
+++ src/index.js
@@ -7,28 +7,55 @@ import { sendToVercelAnalytics } from './vitals';
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 import { ConnectKitProvider, ConnectKitButton, } from "connectkit";
 
-const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet, polygon, optimism, arbitrum], [publicProvider()])
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum
+  ],
+  [
+    alchemyProvider({
+      apiKey: process.env.REACT_APP_ALCHEMY_ID
+    }),
+    publicProvider()
+  ]);
+
+const appName = 'connectkit-demo';
+const projectId = process.env.REACT_APP_WALLETCONNECT_PROJECT_ID;
+const jsonRpcUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_ID}`;
+
 const config = createConfig({
   connectors: [
     new MetaMaskConnector({
-      chains,
       shimDisconnect: false,
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName,
+        jsonRpcUrl
+      }
     }),
     new WalletConnectConnector({
       chains,
       options: {
-        appName: 'connectkit-demo',
-        jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_ID}`,
-        projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID
+        appName,
+        jsonRpcUrl,
+        projectId
       }
     }),
-    new InjectedConnector({ chains }),
+    new InjectedConnector({
+      chains
+    }),
   ],
   publicClient,
   webSocketPublicClient,
@@ -40,6 +67,7 @@ const config = createConfig({
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
+
 root.render(
   <React.StrictMode>
     <WagmiConfig config={config}>
@@ -47,7 +75,6 @@ root.render(
         <ConnectKitButton />
       </ConnectKitProvider>
     </WagmiConfig>
-    <div>home</div>
   </React.StrictMode>,
 );
 
